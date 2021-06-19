f96f8dd892ab4fbd536eb75955e2e33ab786e80b
Tazrian Zaman Tushti <73343497+Tazrian1999@users.noreply.github.com>
2021-06-19 23:30:17 +0600

    LabTask3

---
+++ LabTask3/Profile Picture.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PROFILE PICTURE</title>
+</head>
+<body>
+  
+<form action="Upload Picture.php" method="post" enctype="multipart/form-data">
+<fieldset>
+  <legend> Select image to upload : </legend>
+  <img src="image.png" alt="picture"><br><br>
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <br><br>
+  <hr><br>
+  <input type="submit" value="submit" name="submit">
+</form>
+</fieldset>
+ 
+</body>
+</html>
